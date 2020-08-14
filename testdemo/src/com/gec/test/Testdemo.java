e2a67607b2bc46d4d1aa1eb36d3e1f6e9cbf0651
823788814 <823788814@qq.com>
2020-08-14 20:26:11 +0800

    作者张三第一次提交

---
+++ testdemo/src/com/gec/test/Testdemo.java
@@ -0,0 +1,14 @@
+package com.gec.test;
+
+public class Testdemo {
+
+    //我是团队开发人员  张三  我写了一个insert()
+    public void insert(){
+        System.out.println("public void insert()");
+    }
+
+    //我是团队开发人员  张三  我写了一个add()
+    public void add(){
+        System.out.println("public void add()");
+    }
+}
