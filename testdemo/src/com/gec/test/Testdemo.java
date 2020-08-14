58f7133d71f5ac3200e330053e4f67b772d2b4b3
823788814 <823788814@qq.com>
2020-08-14 20:48:48 +0800

    李四第一次提交

--- testdemo/src/com/gec/test/Testdemo.java
+++ testdemo/src/com/gec/test/Testdemo.java
@@ -4,11 +4,20 @@ public class Testdemo {
 
     //我是团队开发人员  张三  我写了一个insert()
     public void insert(){
+
         System.out.println("public void insert()");
     }
 
     //我是团队开发人员  张三  我写了一个add()
     public void add(){
+
         System.out.println("public void add()");
     }
+
+    //我是团队开发人员  李四  我写了一个update()
+
+    public void update(){
+        System.out.println("public void update()");
+    }
+
 }
